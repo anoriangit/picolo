8980410476e032a447da05d053a80f6ed32e1759
Anoria Dev <anoriangit@users.noreply.github.com>
2025-05-22 12:33:33 +0200

    dma update

--- main.c
+++ main.c
@@ -132,18 +132,13 @@ bool display_timer_callback(__unused struct repeating_timer *t) {
 uint ztick_sm;
 
 // Z80 clock ticking
-// Note the pio program currently is hard coded to tick the Z80 at 10 Hz
-void start_z80() {
+// Note that we are running this on PIO0 (which is also used by PICODVI)
+int start_z80(float tick_freq) {
 
-	static const uint clock_pin = 28;
+	static const uint clock_pin = 27;
 
-	// 4000:tick at 5Hz (2000 for 10Hz) (15 ~ 30Hz) (5 ~ 300Hz) (1 ~ 1KHz)
-	// new: 5000->124Hz, 10000->250Hz 
-	// static const float pio_freq = 1000;		
-	static const float pio_freq = 2000;		
-
-    // Choose PIO instance (0 or 1)
-    PIO pio = pio1;
+	// Choose PIO instance (0 or 1)
+    PIO pio = pio0;
 
     // Get first free state machine in PIO 1
     ztick_sm = pio_claim_unused_sm(pio, true);
@@ -151,8 +146,11 @@ void start_z80() {
 
     // Add PIO program to PIO instruction memory. SDK will find location and
     // return with the memory offset of the program.
-    uint offset = pio_add_program(pio, &ztick_program);
-	Con_printf("ztick program offset: %d\n", offset);
+    int offset = pio_add_program(pio, &ztick_program);
+	if(offset >= 0)
+		Con_printf("ztick program offset: %d\n", offset);
+	else
+		return offset;
 
 	/*
 	The PIO state machine’s clock frequency is:
@@ -160,7 +158,7 @@ void start_z80() {
 	*/
 
     // Calculate the PIO clock divider
-    float div = (float)clock_get_hz(clk_sys) / pio_freq;
+    float div = (float)clock_get_hz(clk_sys) / tick_freq;
 	Con_printf("ztick clock div: %f\n", div);
 
     // Initialize the program using the helper function in our .pio file
@@ -168,24 +166,53 @@ void start_z80() {
 
     // Start running our PIO program in the state machine
     pio_sm_set_enabled(pio, ztick_sm, true);
+	return 0;
 
 }
 
 PIO z80_pio;
 uint z80_sm;
-int z80_dma;
+int read_addr_dma = -1;
+int read_ram_dma = -1;
 
 #define SRAM_SIZE 256
-uint8_t sram[SRAM_SIZE];
+// Align SRAM on 256-byte (8-bit) boundary
+uint8_t SRAM[SRAM_SIZE] __attribute__((aligned(256)));
 
-void process_z80() {
+
+
+void __not_in_flash_func(dma_irq_handler_address)(void) {
+
+    uint32_t addr = (uint32_t)dma_hw->ch[read_ram_dma].al3_read_addr_trig;
+    // uint16_t value = *((uint16_t *)addr);
+    // DPRINTF("DMA ADDR: $%x, VALUE: $%x\n", addr, value);
+
+    Con_printf("DMA IRQ: addr:%u\n", addr);
+    //Con_printf(".");
+
+	// Clear the interrupt request for the dma channel
+    // Clear the interrupt request.
+    dma_hw->ints1 = 1u << read_addr_dma;
+
+    // Restart the DMA
+    //    dma_channel_start(1);
+}
+
+int process_z80() {
+
+    // Initialize SRAM with a contiguous sequence of "JR 6" relative jumps
+    for (int i = 0; i < SRAM_SIZE; i+=2){ 
+		SRAM[i] = 0x18;
+		SRAM[i+1] = 0x6;
+	}
+
 	// Choose PIO instance and claim a state machine
     z80_pio = pio1;
     z80_sm = pio_claim_unused_sm(z80_pio, true);
 	Con_printf("zproc_sm: %d\n", z80_sm);
 
     // Load the PIO program
-    uint offset = pio_add_program(z80_pio, &zproc_program);
+    int offset = pio_add_program(z80_pio, &zproc_program);
 	Con_printf("zproc program offset: %d\n", offset);
 
     // Initialize the PIO state machine
@@ -197,74 +224,87 @@ void process_z80() {
     pio_sm_restart(z80_pio, z80_sm);
  	
     // DMA
-    z80_dma = dma_claim_unused_channel(true);
-	Con_printf("z80_ram dma: %d\n", z80_sm);
+    // Claim the first available DMA channel for read_addr channel
+	read_addr_dma = dma_claim_unused_channel(true);
+   	Con_printf("DMA read_addr: %d\n", read_addr_dma);
+    if (read_addr_dma == -1) {
+        Con_printf("DMA channel error read_addr\n");
+        return -1;
+    }
 
-    dma_channel_config dma_config = dma_channel_get_default_config(z80_dma);
-    channel_config_set_transfer_data_size(&dma_config, DMA_SIZE_8);
-    channel_config_set_read_increment(&dma_config, false);
-    channel_config_set_write_increment(&dma_config, false);
-    channel_config_set_dreq(&dma_config, pio_get_dreq(z80_pio, z80_sm, false));
-    dma_channel_configure(
-        z80_dma,
-        &dma_config,
-        &z80_pio->txf[z80_sm],
-        sram,
-        1,
+    // Claim another available DMA channel for RAM lookup channel
+    read_ram_dma = dma_claim_unused_channel(true);
+   	Con_printf("DMA read_ram: %d\n", read_ram_dma);
+    if (read_ram_dma == -1) {
+        Con_printf("DMA channel error read_ram\n");
+        return -1;
+    }
+
+    // DMA configuration
+    // Lookup RAM dma: the address of the data to read is injected from the
+    // chained previous DMA channel (read_addr_dma) into the read address trigger register.
+    // This DMA does the lookup and pushes the 8 bit result into the TX FIFO
+
+    dma_channel_config cdmaLookup = dma_channel_get_default_config(read_ram_dma);
+    channel_config_set_transfer_data_size(&cdmaLookup, DMA_SIZE_8);
+    channel_config_set_read_increment(&cdmaLookup, false);
+    channel_config_set_write_increment(&cdmaLookup, false);
+    channel_config_set_dreq(&cdmaLookup, pio_get_dreq(z80_pio, z80_sm, true)); // dreq true=send to sm, false=read from sm
+    //channel_config_set_chain_to(&cdmaLookup, read_addr_dma);
+
+	/*
+	Configure all DMA parameters and optionally start transfer.
+	static void dma_channel_configure (
+		uint channel, 
+		const dma_channel_config *config, 
+		volatile void *write_addr, 
+		const volatile void *read_addr, 
+		uint transfer_count, 
+		bool trigger)
+	*/
+
+	dma_channel_configure(
+        read_ram_dma,
+        &cdmaLookup,
+        &z80_pio->txf[z80_sm],		// write to
+        SRAM,						// read from: this is provided by the chained read addr channel
+        1,							// transfer count
         false
-    );
+	); 
 
-	// push pin mask to the tx fifo
-    pio_sm_put_blocking(z80_pio, z80_sm, 0b00000000111);
+    // Read ADDRESS dma: the address to read from the simulated RAM is obtained from the FIFO
+    // and injected into the read address trigger register of the lookup data DMA channel
+    // chained.
+    dma_channel_config cdma = dma_channel_get_default_config(read_addr_dma);
+    channel_config_set_transfer_data_size(&cdma, DMA_SIZE_32);
+    channel_config_set_read_increment(&cdma, false);
+    channel_config_set_write_increment(&cdma, false);
+    channel_config_set_dreq(&cdma, pio_get_dreq(z80_pio, z80_sm, false));
+    //channel_config_set_chain_to(&cdmaLookup, read_ram_dma);
+
+	dma_channel_configure(
+        read_addr_dma,
+        &cdma,
+        &dma_hw->ch[read_ram_dma].al3_read_addr_trig,	// write to
+        &z80_pio->rxf[z80_sm],							// read from
+        1,												// transfers count
+        false
+	);
+
+	// DEBUG: irq handler for read_addr_dma
+#if 0
+    dma_channel_set_irq1_enabled(read_addr_dma, true);
+    irq_set_exclusive_handler(DMA_IRQ_1, dma_irq_handler_address);
+    irq_set_enabled(DMA_IRQ_1, true);
+#endif
+
+	// push 24 most significant bits of the base address of SRAM
+	pio_sm_put(z80_pio, z80_sm, (uintptr_t) SRAM>>8);
 
     // Start running our PIO program in the state machine
     pio_sm_set_enabled(z80_pio, z80_sm, true);
 
-}
-
-// ------------------------------------------------
-// new version of z80_ram
-
-
-void start_z80_ram() {
-
-	Con_printf("Constants:\n PIN_COUNT=%d\n RD_PIN=%d\n ADDR_OE_PIN=%d\n DATA_OE_PIN=%d\n DELAY_COUNT=%d\n DELAY_LOOPS=%d\n",
-           z80_ram_PIN_COUNT, z80_ram_RD_PIN, z80_ram_ADDR_OE_PIN, z80_ram_DATA_OE_PIN,
-           z80_ram_DELAY_COUNT, z80_ram_DELAY_LOOPS);
-
-    // Initialize SRAM
-    for (int i = 0; i < SRAM_SIZE; i++) 
-		sram[i] = 0x00;
-
-	// example
-    //sram[0] = 0xC3; sram[1] = 0x00; sram[2] = 0x00; // JP 0
-
-    // RAM PIO (SM0)
-    z80_pio = pio1;
-    z80_sm = pio_claim_unused_sm(z80_pio, true);
-	Con_printf("z80_ram sm: %d\n", z80_sm);
-
-	uint offset_ram = pio_add_program(z80_pio, &z80_ram_program);
-    z80_ram_program_init(z80_pio, z80_sm, offset_ram);
-
-    // DMA
-    z80_dma = dma_claim_unused_channel(true);
-	Con_printf("z80_ram dma: %d\n", z80_sm);
-
-    dma_channel_config dma_config = dma_channel_get_default_config(z80_dma);
-    channel_config_set_transfer_data_size(&dma_config, DMA_SIZE_8);
-    channel_config_set_read_increment(&dma_config, false);
-    channel_config_set_write_increment(&dma_config, false);
-    channel_config_set_dreq(&dma_config, pio_get_dreq(z80_pio, z80_sm, false));
-    dma_channel_configure(
-        z80_dma,
-        &dma_config,
-        &z80_pio->txf[z80_sm],
-        sram,
-        1,
-        false
-    );
-
+	return 0;
 }
 
 void shutdown(void) {
@@ -293,8 +333,8 @@ int __not_in_flash("main") main() {
 	gpio_put(LED_PIN, true);
 
 	// clock the Z80 manually for now
-	gpio_init(28);
-	gpio_set_dir(28, GPIO_OUT);
+	//gpio_init(28);
+	//gpio_set_dir(28, GPIO_OUT);
 
 	// init tinyusb host stack on configured roothub port
   	tusb_init();
@@ -349,17 +389,35 @@ int __not_in_flash("main") main() {
     struct repeating_timer timer;
     add_repeating_timer_ms(-20, display_timer_callback, NULL, &timer);
 
-	// start the Z80
+	// ---------------------------------------
+	// DMA setup
+	// Adjust Channel 1’s write address to include SRAM base (optional buffer for testing)
+    //addr_buffer = (uint32_t)SRAM; // Initialize with SRAM base
+    //dma_channel_set_read_addr(read_ram_dma, &addr_buffer, false);
+    //dma_channel_set_write_addr(read_addr_dma, &dma_hw->ch[read_ram_dma].al3_read_addr_trig, false);
+    // Start DMA chain
+    // dma_channel_start(read_addr_dma);
+	
+	
+	// start the Z80 ticking
+	int error = 0;
 	process_z80();
-	start_z80();
-	//start_z80_ram();
+
+	// 1000->35Hz (with 60 wait cycles in ztick.pio)
+	// 5000->124Hz, 10000->250Hz 
+	static const float tick_freq = 1000;		
+	if(start_z80(tick_freq) < 0) {
+		Con_printf("Error starting Z80 clock\n");
+		error = 1;
+	}
+
 
 	uint32_t n_loops = 0;
 
 	// main loop
 	while (1) {
 
-		// TEST TEST TEST
+		static uint8_t last_addr = 0;
 
 #if 0
 
@@ -372,18 +430,32 @@ int __not_in_flash("main") main() {
        
 		dma_channel_set_read_addr(z80_dma, &sram[addr], true);
 #else
+		if(!error) {
+		
+		// "Manual" variant (CPU does the transfer)
 		// Wait for data in the RX FIFO
-        uint32_t pin_states = pio_sm_get_blocking(z80_pio, z80_sm);
-	
-		// reconfigure bus pins for output (true)
-	    // pio_sm_set_consecutive_pindirs(z80_pio, z80_sm, 0, 8, true);
+		uint32_t pin_states = pio_sm_get_blocking(z80_pio, z80_sm);
 
-		// Extract the 6-bit value (GP0-GP5)
-        //pin_states &= 0x3F; // Mask to 6 bits
-		static uint8_t last_addr = 0;
-		uint8_t addr = pin_states >> 24;
+		// if ISR is right shift
+		//uint8_t addr = pin_states >> 24;
+		// if ISR is left shift
+		//uint32_t addr = pin_states & 0x000000ff;
+		// if ISR is left shift AND we receive a full address
+		uint32_t addr = pin_states;
 
-		// Print the pin states
+		// transfer the data byte from SRAM to the pio tx fifo, either via dma or manually
+		/* full auto DMA variant does not need any of this */
+#if 1
+		// DMA variant
+		dma_channel_set_read_addr(read_ram_dma, (void*)((uint8_t*)addr), true);
+#else
+		// CPU "manual" variant
+		// for left shift osr we need to move the data byte into the msb of the 32 bit word
+		// uint32_t data = SRAM[addr] << 24;
+		uint32_t data = (*((uint8_t*)addr)) << 24;
+		pio_sm_put_blocking(z80_pio, z80_sm, data);		// move to tx fifo (pulled into osr)
+#endif
+		// DEBUG: Print the pin states
         //Con_printf("A0-A5: 0x%04x\n", pin_states);
 		static uint32_t last_time_ms;
 		uint32_t time_ms = time_us_32() / 1000;
@@ -395,26 +467,19 @@ int __not_in_flash("main") main() {
 #endif
 
 #if 1
-		Con_printf("dT: %u ms - ", time_ms-last_time_ms);
-        Con_printf("A0-A7: %d\n", addr);
+		Con_printf("dT: %03u ms - ", time_ms-last_time_ms);
+        Con_printf("A0-A7: %d\n", addr&0x000000ff);
 #endif
+	
 		last_time_ms = time_ms;
 		last_addr = addr;
 		n_loops++;
 		if(!(n_loops%1000)) {
 			Con_printf(".");
 		}
-		/*
-		Con_printf("GP0-GP5 states: 0x%02x (GP0=%d, GP1=%d, GP2=%d, GP3=%d, GP4=%d, GP5=%d)\n",
-               pin_states,
-               (pin_states >> 0) & 1,
-               (pin_states >> 1) & 1,
-               (pin_states >> 2) & 1,
-               (pin_states >> 3) & 1,
-               (pin_states >> 4) & 1,
-               (pin_states >> 5) & 1); */
-
+	
 #endif
+	}
 		/*
 		char *line = ShellReadInput();	// does not block
 		if(line) {
